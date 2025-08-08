12140bd4c30a9428d8d7aa5e70ba7824bcb82b8e
Joshua Hamilton <hamiltonjoshuadavid@gmail.com>
2025-08-07 22:20:42 -0700

    add unit test for rating 5 on a new card

--- __tests__/basic.test.ts
+++ __tests__/basic.test.ts
@@ -1,4 +1,4 @@
-import { Card } from "../src";
+import { Card, Scheduler } from "../src";
 
 test('should create a card with default values', () => {
 
@@ -10,5 +10,29 @@ test('should create a card with default values', () => {
     expect(card.I).toBe(0);
     expect(card.due instanceof Date).toBe(true);
     expect(card.needsExtraReview).toBe(false);
+
+});
+
+test('test quickstart', () => {
+
+    let card = new Card();
+
+    // card is due immediately upon creation
+    expect(new Date() >= card.due).toBe(true);
+
+    let rating = 5;
+
+    let result = Scheduler.reviewCard(card, 5, card.due); 
+    card = result.card;
+    let reviewLog = result.reviewLog;
+
+    let due = card.due;
+
+    // how much time between when the card is due and now
+    let timeDelta = due.getTime() - (Date.now());
+
+    // card is due in 1 day
+    const MS_PER_DAY = 1000 * 60 * 60 * 24;
+    expect(Math.ceil(timeDelta / MS_PER_DAY)).toEqual(1);
 
 });
